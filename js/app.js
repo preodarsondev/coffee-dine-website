48b29c9e88836120e08f8c1163ced8ba20b99a06
preodarsondev <contactwithpartho@gmail.com>
2024-04-28 23:43:49 +0600

    customized the nav button part

--- js/app.js
+++ js/app.js
@@ -6,3 +6,19 @@ window.addEventListener('load', function () {
 document.querySelector('.navbtn').addEventListener('click', function () {
     document.querySelector('.nav-item').classList.toggle('nav-show')
 })
+
+//video play/pause
+document.querySelector('.video-switch').addEventListener('click', function(){
+    let btn = document.querySelector('.video-switch-btn');
+    if (!btn.classList.contains('play-pause')){
+        btn.classList.add('play-pause');
+        document.querySelector('.video-items').pause();
+    }
+    else{
+        btn.classList.remove('play-pause');
+        document.querySelector('.video-items').play();
+    }
+})
+
+
+
