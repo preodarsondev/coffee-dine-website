5d4af3eb8cf6a971b5e8555d7c767c4c1aff7918
preodarsondev <contactwithpartho@gmail.com>
2024-04-28 20:42:40 +0600

    customized the pre loader and nav

--- js/app.js
+++ js/app.js
@@ -2,3 +2,7 @@
 window.addEventListener('load', function () {
     document.querySelector('.pre-loader').style.display = "none";
 })
+//nav
+document.querySelector('.navbtn').addEventListener('click', function () {
+    document.querySelector('.nav-item').classList.toggle('nav-show')
+})
