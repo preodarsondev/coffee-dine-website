a42a29b66f05d38c5ed3beb5d4912effcfe3cf3d
preodarsondev <contactwithpartho@gmail.com>
2024-05-03 20:44:59 +0600

    customized the drink form section

--- js/app.js
+++ js/app.js
@@ -33,13 +33,24 @@ function eventListeners(){
 
         let value = ui.checkEmpty(firstName, lastName, inputEmail);
         if(value){
+
+            let Customer = new customer(firstName, lastName, inputEmail);
+            console.log(Customer);
+            ui.addCustomer(Customer)
             ui.showfeedback('customer added to the list', 'success')
+            ui.clearFields();
+            
+
         }
         else{
             ui.showfeedback('Please fill all the fields', 'error');
         }
     })
 }
+
+
+
+
 
 //constructor function
 function UI(){
@@ -77,40 +88,34 @@ UI.prototype.removeAlert = function(type){
         document.querySelector('.drink-form-feedback').classList.remove(type);
     }, 3000);
 }
+//add customer
+UI.prototype.addCustomer= function(customer){
+    const image = [1,2,3,4,5];
+    const random = Math.floor(Math.random()*image.length);
+    console.log(random);
+    const div = document.createElement('div');
+    div.classList.add('person');
+
+    div.innerHTML =`<img src="./image/person-${random}.jpeg" alt="Jon Doe" class="demo-person-img">
+    <h3 class="person-firstname">${customer.firstName}</h3>
+    <h3 class="person-lastname">${customer.lastName}</h3>`
+    document.querySelector('.drink-card-list').appendChild(div)
+    
+}
 
 
+//clear field
+UI.prototype.clearFields = function(){
+    document.querySelector('.firstname').value = '';
+    document.querySelector('.laststname').value = '';
+    document.querySelector('.email').value = '';
+}
 
-// submit the form
 
-
-//     let value = ui.checkEmpty(name, lastName, email)
-
-//     if (value) {
-//       let customer = new Customer(name, lastName, email)
-//       console.log(customer);
-//       ui.addCustomer(customer)
-//       ui.showFeedback('custumer added to the list', 'success')
-//       ui.clearFields()
-
-//     }
-//     else {
-//       ui.showFeedback('some form values empty', 'error')
-//     }
-
-//   })
-
-//   UI.prototype.showFeedback = function (text, type) {
-//     const feedback = document.querySelector('.drink-form__feedback');
-//     if (type === 'success') {
-//       feedback.classList.add('success');
-//       feedback.innerText = text;
-//       this.removeAlert('success');
-//     }
-//     else if (type === 'error') {
-  
-//       feedback.classList.add('error');
-//       feedback.innerText = text;
-//       this.removeAlert('error');
-//     }
-//   }
+//customer list
+function customer(firstName, lastName, inputEmail){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.inputEmail = inputEmail;
+}
 
